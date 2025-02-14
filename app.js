18fede1ca222e11b6587800b97233006d0ee69f2
Uniqcle <uniqcle@yandex.ru>
2025-02-14 21:37:50 +0700

    auth

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require("express"),
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+///////////////////////////////////////////////////
+// routes
+///////////////////////////////////////////////////
 app.get("/", (req, res) => {
   console.log(req.query);
   res.send("search: " + req.query.search);
@@ -19,6 +24,34 @@ app.get("/catalog/:id/section/:part", (req, res) => {
   res.send(info);
 });
 
+///////////////////////////////////////////////////
+// auth
+///////////////////////////////////////////////////
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const users = [
+    { login: "mike", password: "12345" },
+    {
+      login: "eduard",
+      password: "12345",
+    },
+  ];
+
+  let user = users.find((user) => {
+    return user.login === req.body.login && user.password === req.body.password;
+  });
+
+  if (user) {
+    res.json({ message: "login" });
+  } else {
+    res.status(400).json({ message: "incorrect login or password" });
+  }
+});
+
+///////////////////////////////////////////////////
+// chaning 
+///////////////////////////////////////////////////
 app
   .route("/user")
   .get((req, res) => {
