7cca658bb1c1803dd39fc2eeee147f7ee7f07b2f
Uniqcle <uniqcle@yandex.ru>
2025-02-14 21:08:39 +0700

    route params

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello, world!!!");
 });
 
+
+app.post("/", (req, res) => {
+  res.send("POST method");
+});
+
+app.get("/catalog/:id/section/:part", (req, res) => {
+  const info = "catalog: " + req.params.id + " section: " + req.params.part;
+  res.send(info);
+});
+
+
 app.listen(3000, () => console.log("server running...3000"));
